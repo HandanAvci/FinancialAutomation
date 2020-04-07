c67dd82744e51db7789a8809b8f8de235635e27f
nefer <nefer@LAPTOP-1RMVF318.home>
2020-04-07 14:06:35 -0400

    UPDATE PROJECT

--- FinancialAutomation/src/test/java/com/financialAutomation/testng/DependsOnMethod.java
+++ FinancialAutomation/src/test/java/com/financialAutomation/testng/DependsOnMethod.java
@@ -6,11 +6,11 @@ public class DependsOnMethod {
 	@Test 
 	public void setupMethod() {
 	 System.out.println("This is setupMethod"); 
-	//Assert.assertTrue(false);
+	
 	
 	}
 	 @Test(dependsOnMethods = { "setupMethod" })
-	
+
 	 public void method2() {
 	 System.out.println("This is login test"); 
 	 } 
